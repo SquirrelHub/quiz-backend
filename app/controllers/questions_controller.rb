84e6090c42e6d7c444ddd300b230202b9c43afbc
Squirrel Dontask <squirrel6946@gmail.com>
2021-04-06 13:20:40 -0500

    Add questions resource paths for create, index, and show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,26 @@
 class QuestionsController < ApplicationController
+  before_action :set_quiz
+  def index
+    @questions = @quiz.questions
+    render json:@questions
+  end
+
+  def show
+    @question = @quiz.questions.find(params[:id])
+    render json:@question
+  end
+
+  def create
+    @question = @quiz.questions.create!(question_params)
+    render json:@question
+  end
+
+  private
+  def question_params
+    params.permit(:question_number, :question_to_answer)
+  end
+
+  def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :quizzes do
-    resource :questions
+    resources :questions
   end
   get 'github', to: redirect("https://github.com/SquirrelHub/quiz-backend")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
